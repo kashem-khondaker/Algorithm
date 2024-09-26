2d4880ef6a70a74dfd3a016aed7929a1c9b9f749
kashem-khondaker <kashem.khondaker.official001@gmail.com>
2024-09-26 17:51:22 +0600

    matrix to adjlist part 2

--- recap module/module 1/matrix_to_adjlist_part2.cpp
+++ recap module/module 1/matrix_to_adjlist_part2.cpp
@@ -8,33 +8,38 @@ int main()
     int mart[n][n];
     for (int i = 0; i < n; i++)
     {
-        for(int j = 0 ; j < n ; j++)
+        for(int j = 0; j < n; j++)
         {
             cin >> mart[i][j];
         }
     }
 
-    vector<int> adjlist[n];
+    vector<pair<int,int>> adjlist[n];
+
     for (int i = 0; i < n; i++)
     {
-        for(int j = 0 ; j < n ; j++)
+        for(int j = 0; j < n ; j++)
         {
-            if (mart[i][j] == 1)
+            if ( ( mart[i][j] > 0 && mart[i][j] != 1 ))
             {
-                adjlist[i].push_back(j);
+                adjlist[i].push_back({j,mart[i][j]});
             }
             
         }
     }
 
+
     for (int i = 0; i < n; i++)
     {
-        cout << i << " -> ";
-        for (int child : adjlist[i])
+        cout << i << " -> " ;
+        for(pair<int,int> node : adjlist[i])
         {
-            cout << child << " ";
+            cout << "{" << node.first << "," << node.second << "}" << " ";
         }
         cout << endl;
     }
+    
+    
+    
     return 0;
 }
