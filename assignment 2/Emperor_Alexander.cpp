f49d6f16728a5a578d1c55797eadec85f580f7c1
kashem-khondaker <kashem.khondaker.official001@gmail.com>
2024-09-28 15:18:53 +0600

    emperor alexander road minimum cost

---
+++ assignment 2/Emperor_Alexander.cpp
@@ -0,0 +1,93 @@
+#include <bits/stdc++.h>
+#define ll long long int
+using namespace std;
+
+const ll N = 1e5 + 5;
+ll parent[N];
+ll group_size[N];
+
+class Edge {
+public:
+    ll u, v, w;
+    Edge(ll u, ll v, ll w) {
+        this->u = u;
+        this->v = v;
+        this->w = w;
+    }
+};
+
+bool cmp(Edge a, Edge b) {
+    return a.w < b.w;
+}
+
+void dsu_initialize(ll n) {
+    for (int i = 1; i <= n; i++) {
+        parent[i] = -1;
+        group_size[i] = 1;
+    }
+}
+
+ll dsu_find(ll node) {
+    if (parent[node] == -1)
+        return node;
+    return parent[node] = dsu_find(parent[node]); // Path compression
+}
+
+void dsu_union_by_size(ll node1, ll node2) {
+    ll leaderA = dsu_find(node1);
+    ll leaderB = dsu_find(node2);
+
+    if (leaderA != leaderB) {
+        // Union by size
+        if (group_size[leaderA] > group_size[leaderB]) {
+            parent[leaderB] = leaderA;
+            group_size[leaderA] += group_size[leaderB];
+        } else {
+            parent[leaderA] = leaderB;
+            group_size[leaderB] += group_size[leaderA];
+        }
+    }
+}
+
+int main() {
+    ll n, e;
+    cin >> n >> e;
+
+    dsu_initialize(n);
+    vector<Edge> edgeList;
+    
+    for (int i = 0; i < e; i++) {
+        ll u, v, w;
+        cin >> u >> v >> w;
+        edgeList.push_back(Edge(u, v, w));
+    }
+    
+    // Sort edges by weight for Kruskal's algorithm
+    sort(edgeList.begin(), edgeList.end(), cmp);
+
+    ll totalCost = 0;
+    ll usedEdges = 0;
+    
+    for (Edge ed : edgeList) {
+        ll leaderU = dsu_find(ed.u);
+        ll leaderV = dsu_find(ed.v);
+        
+        if (leaderU != leaderV) {
+            // If u and v are in different components, add this edge to MST
+            dsu_union_by_size(ed.u, ed.v);
+            totalCost += ed.w;
+            usedEdges++;
+        }
+    }
+    
+    // If we couldn't use exactly N-1 edges, the graph is not connected
+    if (usedEdges != n - 1) {
+        cout << "Not Possible" << endl;
+    } else {
+        // Extra roads are the ones not used in the MST
+        ll removedRoads = e - usedEdges;
+        cout << removedRoads << " " << totalCost << endl;
+    }
+    
+    return 0;
+}
