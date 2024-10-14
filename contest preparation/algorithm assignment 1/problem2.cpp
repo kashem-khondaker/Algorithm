de816a48161f1a24e680ad53fa631d67fd39746a
kashem-khondaker <kashem.khondaker.official001@gmail.com>
2024-10-15 05:50:58 +0600

    problem 2 solve by pair<string,int>

--- contest preparation/algorithm assignment 1/problem2.cpp
+++ contest preparation/algorithm assignment 1/problem2.cpp
@@ -1,56 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class student 
-{
-public:
-    string name;
-    int val;
-
-    student(string name, int val) 
-    {
-        this->name = name;
-        this->val = val;
-    }
-};
-
 // Comparator class
 class cmp {
 public:
-    bool operator()(student a, student b) 
-    {
-        if (a.name != b.name) {
-            return a.name > b.name;  // Ascending order for names
+    bool operator()(const pair<string, int>& a, const pair<string, int>& b) {
+        if (a.first != b.first) {
+            return a.first > b.first;  // Ascending order for names
         }
-        return a.val < b.val;  // Descending order for values if names are the same
+        return a.second < b.second;  // Descending order for values if names are the same
     }
 };
 
-int main() 
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+int main() {
+    ios_base::sync_with_stdio(false);  
+    cin.tie(NULL); 
 
     int n;
     cin >> n;
 
-    priority_queue<student, vector<student>, cmp> pq;
+    priority_queue<pair<string, int>, vector<pair<string, int>>, cmp> pq;
 
-    for (int i = 0; i < n; i++) 
-    {
+    for (int i = 0; i < n; i++) {
         string name;
         int val;
         cin >> name >> val;
-
-        student Student(name, val);
-        pq.push(Student);
+        
+        pq.push({name, val}); 
     }
 
     // Printing elements from priority queue
-    while (!pq.empty()) 
-    {
-        student p = pq.top();
-        cout << p.name << " " << p.val << endl;
+    while (!pq.empty()) {
+        auto p = pq.top();
+        cout << p.first << " " << p.second << endl;
         pq.pop();
     }
 
