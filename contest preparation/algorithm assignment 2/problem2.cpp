49aee15ac9ec6fad5f5c2f3d3469a792e7e808e1
kashem-khondaker <kashem.khondaker.official001@gmail.com>
2024-10-15 09:17:21 +0600

    problem 2 solve assignment problem prefixed sum now updatade version

--- contest preparation/algorithm assignment 2/problem2.cpp
+++ contest preparation/algorithm assignment 2/problem2.cpp
@@ -13,24 +13,19 @@ int main() {
     vector<ll> prefix_sum(n); // Array to store prefix sums
 
     // Input elements of the array
-    for (int i = 0; i < n; i++) {
+    for (ll i = 0; i < n; i++) {
         cin >> arr[i];
     }
 
     // Calculate prefix sums
     prefix_sum[0] = arr[0]; 
-    for (int i = 1; i < n; i++) {
+    for (ll i = 1; i < n; i++) {
         prefix_sum[i] = prefix_sum[i - 1] + arr[i]; 
     }
 
-    vector<ll> v;
-    for (int i = 0; i < n; i++) {
-        v.push_back(prefix_sum[i]);
-    }
-    reverse(v.begin(),v.end());
-    for (int val : v)
-    {
-        cout << val << " ";
+    // Print the prefix sums in reverse order
+    for (ll i = n - 1; i >= 0; i--) {
+        cout << prefix_sum[i] << " ";
     }
     cout << endl; 
 
